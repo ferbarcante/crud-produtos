2ca09cdd239b7dbf92bfe43dec08d8dc76385059
ferbarcante <barcantef@gmail.com>
2024-10-20 17:12:57 -0300

    conn

---
+++ Conn.php
@@ -0,0 +1,14 @@
+<?php
+
+class Connn {
+    //ṕadrão singleton
+    private static $instance;
+
+    public static function getConn(){
+        if(!isset(self::$instance)):
+            self::$instance = new PDO('mysql:host=localhost;dbname=test', 'root', '');
+        else:
+            return self::$instance;
+        endif;
+    }
+}
---
+++ ProdutoDAO.php
@@ -0,0 +1,19 @@
+<?php
+
+class ProdutoDAO{
+    public function create(Produto $produto){
+
+    }
+
+    public function update(Produto $produto){
+
+    }
+
+    public function delete(Produto $produto){
+
+    }
+
+    public function findAll(){
+
+    }
+}
